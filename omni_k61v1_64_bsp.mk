5349b91fa7cbdccabc840d469ce987b407280ac8
SUGINACH <40427125+SUGINACH@users.noreply.github.com>
2021-12-28 12:06:39 +0200

    Add files via upload

---
+++ omni_k61v1_64_bsp.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from k61v1_64_bsp device
+$(call inherit-product, device/duoqin/k61v1_64_bsp/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := k61v1_64_bsp
+PRODUCT_NAME := omni_k61v1_64_bsp
+PRODUCT_BRAND := Qin
+PRODUCT_MODEL := F21 Pro
+PRODUCT_MANUFACTURER := duoqin
